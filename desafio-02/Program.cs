e1f3220eb9d1495e2ce100cbe5f1e95a925432dd
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-12-05 23:57:27 -0300

    Resolução do segundo desafio

---
+++ desafio-02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Linq;
+using System;
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        int qt = int.Parse(Console.ReadLine());
+        string[] v = new string[2];
+        for (int count = 1; count <= qt; count++)
+        {
+            v = Console.ReadLine().Split(" ");
+            string a = v[0];
+            string b = v[1];
+
+            if (b.Length > a.Length)
+                Console.WriteLine("nao encaixa");
+            else if (a.EndsWith(b))
+                Console.WriteLine("encaixa");
+            else
+                Console.WriteLine("nao encaixa");
+
+        }
+
+    }
+
+}
